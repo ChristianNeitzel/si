36e880dfb1154d92cd39cecf0ac0d3098dc2170a
ChristianNeitzel <pg52526@uminho.pt>
2024-09-24 13:10:28 +0100

    Adding my co-authorship to the package

--- src/si/__init__.py
+++ src/si/__init__.py
@@ -1,9 +1,9 @@
-__author__ = "João Capela"
-__credits__ = ["João Capela", "João Correia, Fernando Cruz", "Vítor Pereira"]
+__author__ = "Christian Neitzel"
+__credits__ = ["Christian Neitzel"]
 __license__ = "Apache License 2.0"
 __version__ = "0.0.1"
-__maintainer__ = "João Capela"
-__email__ = "joao.capela@ceb.uminho.pt"
+__maintainer__ = "Christian Neitzel"
+__email__ = "pg52526@uminho.pt"
 
 # -----------------------------------------------------------------------------------
 # This package is heavily inspired and adapted from
