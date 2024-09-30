e881ea8b7f3f8e1e0de2d1288b26d8966e2ffdd5
ChristianNeitzel <pg52526@uminho.pt>
2024-09-30 22:59:06 +0100

    Added KMeans, SelectKBest, euclidean_distance, f_classification and their unittests

--- src/si/feature_selection/select_k_best.py
+++ src/si/feature_selection/select_k_best.py
@@ -1,3 +1,9 @@
+import numpy as np
+
+from si.base.estimator import Estimator
+from si.base.transformer import Transformer
+from si.data.dataset import Dataset
+
 class SelectKBest(Transformer):
     """
     Parameters:
@@ -10,3 +16,22 @@ class SelectKBest(Transformer):
         _fit: estimates the F and p values for each feature using the scoring_func; returns self
         _transform: selects the top k features with the highest F value and returns the selected X
     """
+    def __init__(self, score_func: callable, k: int, **kwargs):
+        super().__init__(**kwargs)
+        self.score_func = score_func
+        self.k = k
+        self.F = None
+        self.p = None
+
+    def _fit(self, dataset: Dataset) -> 'SelectKBest':
+            self.F, self.p = self.score_func(dataset)
+
+            return self
+
+    def _transform(self, dataset: Dataset) -> Dataset:
+        idx = np.argsort(self.F)
+        mask = idx[-self.k:]  # Mask to obtain the top k features with the highest F value
+        new_X = dataset.X[:, mask]
+        new_features = dataset.features[mask]
+
+        return Dataset(X=new_X, features=new_features, y=dataset.y, label=dataset.label)
--- src/si/statistics/f_classification.py
+++ src/si/statistics/f_classification.py
@@ -1,7 +1,21 @@
-def f_classification(dataset):
+import scipy
+
+from si.data.dataset import Dataset
+
+def f_classification(dataset: Dataset) -> tuple:
     """
     Args: 
         dataset: dataset object
     Output:
         tuple with F values + tuple with p values
     """
+    classes = dataset.get_classes()         # Obtain classes from the dataset via the function get_classes() from the "from si.data.dataset import Dataset" import
+
+    groups = []                             # List of arrays that will contain our attributes
+    for class_ in classes:                  # Note: the term 'class' cannot be defined here as it is already a natively defined python class.
+        mask = dataset.y == class_
+        group = dataset.X[ mask , :]
+        groups.append(group)
+
+    return scipy.stats.f_oneway(*groups)
+
