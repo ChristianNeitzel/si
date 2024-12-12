[FIX] self.n_unitest to self.n_units in DenseLayer class; [ADD] Dropout, Tanh and Softmax layers unittests

src/si/neural_networks/layers.py
@@ -37,7 +37,6 @@ class DenseLayer(Layer):
     """
     Dense layer of a neural network.
     """
-
     def __init__(self, n_units: int, input_shape: tuple = None):
         """
         Initialize the dense layer.
@@ -70,7 +69,7 @@ class DenseLayer(Layer):
         n_features = self.input_shape()[0]
 
         # Initialize weights from a 0 centered uniform distribution [-0.5 and 0.5)
-        self.weights = np.random.rand(n_features, self.n_unitest) - 0.5
+        self.weights = np.random.rand(n_features, self.n_units) - 0.5
 
         # Initialize biases to 0s
         self.biases = np.zeros((1, self.n_units))

tests/unit_tests/test_dense_layer.py
@@ -9,11 +9,10 @@ import os
 
 from si.io.data_file import read_data_file
 from si.model_selection.split import train_test_split
-from si.neural_networks.layers import DenseLayer
+from si.neural_networks.layers import DenseLayer, Dropout
 from si.neural_networks.optimizers import Optimizer
 
 class MockOptimizer(Optimizer):
-
     def __init__(self, learning_rate):
         super().__init__(learning_rate)
 
@@ -21,24 +20,19 @@ class MockOptimizer(Optimizer):
         return w - self.learning_rate * grad_loss_w
 
 class TestDenseLayer(TestCase):
-
     def setUp(self):
-        
         self.csv_file = os.path.join(DATASETS_PATH, 'breast_bin', 'breast-bin.csv')
-
         self.dataset = read_data_file(filename=self.csv_file, label=True, sep=",")
-
         self.train_dataset, self.test_dataset = train_test_split(self.dataset)
 
     def test_forward_propagation(self):
-
         dense_layer = DenseLayer(n_units=30)
         dense_layer.set_input_shape((self.dataset.X.shape[1], ))
         dense_layer.initialize(MockOptimizer(0.001))
         output = dense_layer.forward_propagation(self.dataset.X, training=False)
+
         self.assertEqual(output.shape[0], self.dataset.X.shape[0])
         self.assertEqual(output.shape[1], 30)
-
 
     def test_backward_propagation(self):
         dense_layer = DenseLayer(n_units=30)
@@ -46,5 +40,37 @@ class TestDenseLayer(TestCase):
         dense_layer.initialize(MockOptimizer(learning_rate=0.001))
         dense_layer.forward_propagation(self.dataset.X, training=True)
         input_error = dense_layer.backward_propagation(output_error=np.random.random((self.dataset.X.shape[0], 30)))
+
         self.assertEqual(input_error.shape[0], self.dataset.X.shape[0])
         self.assertEqual(input_error.shape[1], 9)
+
+
+# Evaluation Exercise 12: Unittest of Dropout layer implementation.
+class TestDropoutLayer(TestCase):
+    def setUp(self):
+        self.csv_file = os.path.join(DATASETS_PATH, 'breast_bin', 'breast-bin.csv')
+        self.dataset = read_data_file(filename=self.csv_file, label=True, sep=",")
+        self.train_dataset, self.test_dataset = train_test_split(self.dataset)
+
+        self.dropout_layer = Dropout(probability=0.3)
+
+    def test_forward_propagation_training(self):
+        output = self.dropout_layer.forward_propagation(self.train_dataset.X, training=True)    # Forward propagation in training mode
+
+        self.assertEqual(output.shape, self.train_dataset.X.shape)
+        self.assertGreater(np.sum(self.dropout_layer.mask == 0), 0)                             # Check that the mask is applied
+
+    def test_forward_propagation_inference(self):
+        output = self.dropout_layer.forward_propagation(self.train_dataset.X, training=False)   # Forward propagation in inference mode
+        np.testing.assert_array_equal(output, self.train_dataset.X)                             # Check that output matches input
+
+    def test_backward_propagation(self):
+        self.dropout_layer.forward_propagation(self.train_dataset.X, training=True)             # Forward propagation in training mode to generate the mask
+
+        output_error = np.random.random(self.train_dataset.X.shape)                             # Generate mock output error with same shape as input
+        input_error = self.dropout_layer.backward_propagation(output_error)                     # backward propagation
+
+        self.assertEqual(input_error.shape, output_error.shape)                                 # Check that input error has same shape as output error
+
+        masked_elements = np.all(input_error[self.dropout_layer.mask == 0] == 0)
+        self.assertTrue(masked_elements)                                                        # Check that mask is applied to the input error